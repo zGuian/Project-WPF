3ae97805b7172f67271d92a3e247f6b7ec502187
Guian Rocha <Guianmarcos32@gmail.com>
2024-09-24 16:00:48 -0300

    Structured project

---
+++ src/Register.Desktop.Domain/Handlers/RegisterEquipmentEntryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Register.Desktop.Domain.Commands.Requests;
+using Register.Desktop.Domain.Commands.Responses;
+
+namespace Register.Desktop.Domain.Handlers
+{
+    public class RegisterEquipmentEntryHandler : IRequestHandler<RegisterEquipmentEntryRequest, RegisterEquipmentEntryResponse>
+    {
+        public Task<RegisterEquipmentEntryResponse> Handle(RegisterEquipmentEntryRequest request, CancellationToken cancellationToken)
+        {
+            // VERIFICA SE O CLIENTE ESTA CADASTRADO
+            // VALIDA DADOS
+            // INSERIR CLIENTE
+            // ACIONA OUTROS SERVIÇOS
+            // MANIPULA DADOS ANTES DE RETORNA O RESPONSE
+            throw new NotImplementedException();
+        }
+    }
+}
